95b9cb137822a8f81884d1616344588924eb0e39
Samaki01 <andrea.quaglia01@gmail.com>
2025-02-27 11:42:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let lastOp = '';
+
+function button(op) {
+    let input = document.getElementById('input');
+
+    if (input.value == '0' && Number.isInteger(op)) {
+        input.value = op;
+    } else {
+        if (Number.isInteger(op)) {
+            input.value += op
+        } else {
+            if (op == '.') {
+                input.value += '.';
+            } else {
+                if (lastOp != '') {
+                    let operands = input.value.split(' ')
+                    switch(lastOp) {
+                        case '+':
+                            input.value = Number(operands[0]) + Number(operands[2]) + ' ' + op + ' ';
+                            break;
+                        case '-':
+                            input.value = Number(operands[0]) - Number(operands[2]) + ' ' + op + ' ';
+                            break;
+                        case '/':
+                            input.value = Number(operands[0]) / Number(operands[2]) + ' ' + op + ' ';
+                            break;
+                        case '*':
+                            input.value = Number(operands[0]) * Number(operands[2]) + ' ' + op + ' ';
+                            break;
+                    }
+                    lastOp = op;
+                } else {
+                    lastOp = op;
+                    input.value += ' ' + lastOp + ' ';
+                }
+            }
+        }
+    }
+}
+
+function buttonCE() {
+    document.getElementById('input').value = 0;
+    lastOp = '';
+}
+
+function buttonEquals() {
+    let input = document.getElementById('input');
+    if (input.value.indexOf('+') > -1) {
+        let nums = input.value.split(" + ");
+        input.value = Number(nums[0]) + Number(nums[1]);
+    } else if (input.value.indexOf('-') > -1) {
+        let nums = input.value.split(" - ");
+        input.value = Number(nums[0]) - Number(nums[1]);
+    } else if (input.value.indexOf('/') > -1) {
+        let nums = input.value.split(" / ");
+        input.value = Number(nums[0]) / Number(nums[1]);
+    } else if (input.value.indexOf('*') > -1) {
+        let nums = input.value.split(" * ");
+        input.value = Number(nums[0]) * Number(nums[1]);
+    }
+    lastOp = '';
+}
