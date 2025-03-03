2f9574cc498f88cc625cd5281203731c0d5b23c4
Samaki01 <andrea.quaglia01@gmail.com>
2025-03-03 11:58:08 +0100

    Change input to display

--- script.js
+++ script.js
@@ -3,35 +3,35 @@ let lastOp = '';
 function button(op) {
     let input = document.getElementById('input');
 
-    if (input.value == '0' && Number.isInteger(op)) {
-        input.value = op;
+    if (input.textContent == '0' && Number.isInteger(op)) {
+        input.textContent = op;
     } else {
         if (Number.isInteger(op)) {
-            input.value += op
+            input.textContent += op
         } else {
             if (op == '.') {
-                input.value += '.';
+                input.textContent += '.';
             } else {
                 if (lastOp != '') {
-                    let operands = input.value.split(' ')
+                    let operands = input.textContent.split(' ')
                     switch(lastOp) {
                         case '+':
-                            input.value = Number(operands[0]) + Number(operands[2]) + ' ' + op + ' ';
+                            input.textContent = Number(operands[0]) + Number(operands[2]) + ' ' + op + ' ';
                             break;
                         case '-':
-                            input.value = Number(operands[0]) - Number(operands[2]) + ' ' + op + ' ';
+                            input.textContent = Number(operands[0]) - Number(operands[2]) + ' ' + op + ' ';
                             break;
                         case '/':
-                            input.value = Number(operands[0]) / Number(operands[2]) + ' ' + op + ' ';
+                            input.textContent = Number(operands[0]) / Number(operands[2]) + ' ' + op + ' ';
                             break;
                         case '*':
-                            input.value = Number(operands[0]) * Number(operands[2]) + ' ' + op + ' ';
+                            input.textContent = Number(operands[0]) * Number(operands[2]) + ' ' + op + ' ';
                             break;
                     }
                     lastOp = op;
                 } else {
                     lastOp = op;
-                    input.value += ' ' + lastOp + ' ';
+                    input.textContent += ' ' + lastOp + ' ';
                 }
             }
         }
@@ -39,24 +39,24 @@ function button(op) {
 }
 
 function buttonCE() {
-    document.getElementById('input').value = 0;
+    document.getElementById('input').textContent = 0;
     lastOp = '';
 }
 
 function buttonEquals() {
     let input = document.getElementById('input');
-    if (input.value.indexOf(" + ") > -1) {
-        let nums = input.value.split(" + ");
-        input.value = Decimal.sum(nums[0], nums[1]);
-    } else if (input.value.indexOf(" - ") > -1) {
-        let nums = input.value.split(" - ");
-        input.value = Decimal.sub(nums[0], nums[1]);
-    } else if (input.value.indexOf(" / ") > -1) {
-        let nums = input.value.split(" / ");
-        input.value = Decimal.div(nums[0], nums[1]);
-    } else if (input.value.indexOf(" * ") > -1) {
-        let nums = input.value.split(" * ");
-        input.value = Decimal.mul(nums[0], nums[1]);
+    if (input.textContent.indexOf(" + ") > -1) {
+        let nums = input.textContent.split(" + ");
+        input.textContent = Decimal.sum(nums[0], nums[1]);
+    } else if (input.textContent.indexOf(" - ") > -1) {
+        let nums = input.textContent.split(" - ");
+        input.textContent = Decimal.sub(nums[0], nums[1]);
+    } else if (input.textContent.indexOf(" / ") > -1) {
+        let nums = input.textContent.split(" / ");
+        input.textContent = Decimal.div(nums[0], nums[1]);
+    } else if (input.textContent.indexOf(" * ") > -1) {
+        let nums = input.textContent.split(" * ");
+        input.textContent = Decimal.mul(nums[0], nums[1]);
     }
     lastOp = '';
 }
